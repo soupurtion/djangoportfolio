f65098dcba4e8e5b10f35b7c1fb2f4a17d099b7f
Sourav Purification <soupurtion@hotmail.com>
2023-09-14 11:16:29 -0600

    defined uri path and view

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+
+
+# Render the HTML template index.html with the data in the context variable.
+   #return HttpResponse('home page of Sourav')
+   return render( request, 'portfolio_app/index.html')
